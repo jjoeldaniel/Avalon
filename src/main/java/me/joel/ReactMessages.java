1d305fe82319639c66e16a583141faae3d13ad0f
Joel <100006388+joelrico@users.noreply.github.com>
2022-05-07 21:18:17 -0700

    Changed Kae auto response to 50% trigger chance

--- src/main/java/me/joel/ReactMessages.java
+++ src/main/java/me/joel/ReactMessages.java
@@ -5,14 +5,10 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
 
 public class ReactMessages extends ListenerAdapter {
 
     private static String kaeResponse() {
-        Random rand = new Random();
 
         ArrayList<String> kaeReply = new ArrayList<>();
         kaeReply.add("Fun fact: Did you know Kae is 4'11?");
@@ -21,20 +17,19 @@ public class ReactMessages extends ListenerAdapter {
         kaeReply.add("Fun fact: Did you know Kae is 4'9?");
         kaeReply.add("Fun fact: Did you know Kae's height changes every second?");
 
-        int num = rand.nextInt(kaeReply.size());
+        int num = Util.randomWithRange(0, kaeReply.size());
 
         return kaeReply.get(num);
     }
 
     private static String spectrumResponse() {
-        Random rand = new Random();
 
         ArrayList<String> spectrumResponse = new ArrayList<>();
         spectrumResponse.add("Fun fact: Did you know Spectrum servers are powered by 3 hamsters on a wheel?");
         spectrumResponse.add("Fun fact: Did you know Spectrum users love to wear wet socks to bed?");
         spectrumResponse.add("Fun fact: Did you know Spectrum users still live in their parents basement?");
 
-        int num = rand.nextInt(spectrumResponse.size());
+        int num = Util.randomWithRange(0, spectrumResponse.size());
 
         return spectrumResponse.get(num);
     }
@@ -51,7 +46,11 @@ public class ReactMessages extends ListenerAdapter {
 
             // Kae
             if (messageSent.contains("kae")) {
-                event.getTextChannel().sendMessage(kaeResponse()).queue();
+                int num = Util.randomWithRange(0, 100);
+                if (num > 50) {
+                    event.getTextChannel().sendMessage(kaeResponse()).queue();
+                }
+
             }
             // Goodnight
             if (messageSent.equalsIgnoreCase("goodnight")) {
