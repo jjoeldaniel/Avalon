4ac20bcde5c4633ccb9f33dead5850baa7cd0e30
Joel <100006388+joelrico@users.noreply.github.com>
2022-05-12 00:38:28 -0700

    Moved 8ball from Commands to FunCommands

--- src/main/java/me/joel/Commands.java
+++ src/main/java/me/joel/Commands.java
@@ -59,44 +59,9 @@ public class Commands extends ListenerAdapter {
                         try {
                             String userPFP = event.getAuthor().getEffectiveAvatarUrl();
                             event.getTextChannel().sendMessage(userPFP).queue();
-                            return;
                         }
                         catch (Exception ignored) {}
                     } // Author
-
-                    // 8Ball
-                    if (botInput[1].equalsIgnoreCase("8ball")) {
-                        Random rand = new Random();
-                        int randomResult = Util.randomWithRange(1, 19);
-
-                        try {
-                            if (!botInput[2].equalsIgnoreCase("")) {
-                                switch (randomResult) {
-                                    case 1 -> event.getTextChannel().sendMessage("It is certain.").queue();
-                                    case 2 -> event.getTextChannel().sendMessage("It is decidedly so.").queue();
-                                    case 3 -> event.getTextChannel().sendMessage("Without a doubt.").queue();
-                                    case 4 -> event.getTextChannel().sendMessage("Yes definitely.").queue();
-                                    case 5 -> event.getTextChannel().sendMessage("You may rely on it.").queue();
-                                    case 6 -> event.getTextChannel().sendMessage("As I see it, yes.").queue();
-                                    case 7 -> event.getTextChannel().sendMessage("Outlook good.").queue();
-                                    case 8 -> event.getTextChannel().sendMessage("Yes.").queue();
-                                    case 9 -> event.getTextChannel().sendMessage("Signs point to yes.").queue();
-                                    case 10 -> event.getTextChannel().sendMessage("Reply hazy, try again.").queue();
-                                    case 11 -> event.getTextChannel().sendMessage("Ask again later.").queue();
-                                    case 12 -> event.getTextChannel().sendMessage("Better not tell you now.").queue();
-                                    case 13 -> event.getTextChannel().sendMessage("Cannot predict now.").queue();
-                                    case 14 -> event.getTextChannel().sendMessage("Concentrate and ask again.").queue();
-                                    case 15 -> event.getTextChannel().sendMessage("Don't count on it.").queue();
-                                    case 16 -> event.getTextChannel().sendMessage("My reply is no.").queue();
-                                    case 17 -> event.getTextChannel().sendMessage("My sources say no.").queue();
-                                    case 18 -> event.getTextChannel().sendMessage("Outlook not so good.").queue();
-                                    case 19 -> event.getTextChannel().sendMessage("Very doubtful.").queue();
-                                }
-                            }
-                        } catch (Exception exception) {
-                            event.getTextChannel().sendMessage("Try entering something.").queue();
-                        }
-                    }
 
                 }
 
