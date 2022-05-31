20082d81098d0b959e7587a2e6957ed4a6395c04
Joel <100006388+joelrico@users.noreply.github.com>
2022-05-30 17:06:43 -0700

    Added comments

--- src/main/java/me/joel/Commands.java
+++ src/main/java/me/joel/Commands.java
@@ -37,7 +37,7 @@ public class Commands extends ListenerAdapter {
                     // Commands list
                     if (botInput[1].equalsIgnoreCase("help")) {
                         User user = event.getAuthor();
-                        // DM's user
+                        // DMs user
                         user.openPrivateChannel().flatMap(channel -> channel.sendMessage(printCommands())).queue();
                     }
 
