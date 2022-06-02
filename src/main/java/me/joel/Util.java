c73c9e85ae2ff008f31234e8db9797cbaad5e08b
Joel <100006388+joelrico@users.noreply.github.com>
2022-06-02 10:43:39 -0700

    Add random color method

--- src/main/java/me/joel/Util.java
+++ src/main/java/me/joel/Util.java
@@ -1,12 +1,22 @@
 package me.joel;
 
+import java.awt.*;
 import java.util.Random;
 
 public class Util {
 
+    // Random number from 0 - i
     public static int randomWithRange(int min, int max) {
         Random rand = new Random();
         return rand.nextInt(min, max);
+    }
+
+    // Random color
+    public static Color randColor() {
+        int num1 = randomWithRange(0, 255);
+        int num2 = randomWithRange(0, 255);
+        int num3 = randomWithRange(0, 255);
+        return new Color(num1, num2, num3);
     }
 
 }
