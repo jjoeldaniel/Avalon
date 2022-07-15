b5753cddbec66b8d4a76fbecf5bc77aafbeaa3fd
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-15 12:07:54 -0700

    Removed unused imports

--- src/main/java/me/joel/ModCommands.java
+++ src/main/java/me/joel/ModCommands.java
@@ -4,14 +4,11 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Channel;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import org.w3c.dom.Text;
 
-import java.util.Collection;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
