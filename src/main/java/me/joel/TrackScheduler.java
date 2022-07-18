8c71320d5472de81ef1f05b376af1c39a2a3a567
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-17 20:39:35 -0700

    `now playing` now functions in VC

--- src/main/java/me/joel/TrackScheduler.java
+++ src/main/java/me/joel/TrackScheduler.java
@@ -27,7 +27,7 @@ public class TrackScheduler extends AudioEventAdapter {
     public void nextTrack() {
         this.audioPlayer.startTrack(this.queue.poll(), false);
         MusicCommands.setSendNowPlaying(true);
-        MusicCommands.sendNowPlaying(audioPlayer.getPlayingTrack(), MusicCommands.returnTextChannel());
+        MusicCommands.sendNowPlaying(audioPlayer.getPlayingTrack(), MusicCommands.returnChannel());
     }
 
     @Override
