9656e7eb0e70a3aa55c25b41ef736eb84a2c13aa
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-21 14:42:20 -0700

    Removed AFK.java and moved to Commands.java

--- src/main/java/me/joel/Commands.java
+++ src/main/java/me/joel/Commands.java
@@ -1,9 +1,11 @@
 package me.joel;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -311,6 +313,69 @@ public class Commands extends ListenerAdapter
 
         }
 
+        if (event.getName().equals("afk"))
+        {
+
+            try
+            {
+
+                Member member = event.getMember();
+                assert member != null;
+
+                // Check for admin/owner
+                if (member.getPermissions().contains(Permission.ADMINISTRATOR) || member.isOwner())
+                {
+
+                    EmbedBuilder builder = new EmbedBuilder()
+                            .setDescription("Owner/Admins cannot use /afk!")
+                            .setColor(Util.randColor());
+
+                    event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+                    return;
+                }
+
+                // Return from AFK
+                if (member.getEffectiveName().startsWith("(AFK)"))
+                {
+                    String user = Objects.requireNonNull(event.getMember()).getEffectiveName();
+                    StringBuilder username = new StringBuilder()
+                            .append(user)
+                            .delete(0, 5);
+                    member.modifyNickname(username.toString()).queue();
+
+                    EmbedBuilder builder = new EmbedBuilder()
+                            .setDescription("Welcome back, " + event.getMember().getAsMention() + "!")
+                            .setColor(Util.randColor());
+
+                    event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+                    return;
+                }
+                if (!member.getEffectiveName().startsWith("(AFK)"))
+                {
+                    String newName = "(AFK) " + member.getEffectiveName();
+
+                    EmbedBuilder builder = new EmbedBuilder()
+                            .setDescription("Your AFK status has been set, " + event.getMember().getAsMention() + "!")
+                            .setColor(Util.randColor());
+
+                    member.modifyNickname(newName).queue();
+                    event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+                }
+
+            }
+            // Exception Catch
+            catch (Exception exception)
+            {
+                EmbedBuilder builder = new EmbedBuilder()
+                        .setDescription("Unknown error occurred, try again later!")
+                        .setFooter("Make sure PawBots role is set as high as possible in the role hierarchy if this error continues to occur!")
+                        .setColor(Util.randColor());
+
+                event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+            }
+
+        }
+
     }
 
     @Override
@@ -501,6 +566,32 @@ public class Commands extends ListenerAdapter
                             .queue();
                 }
             }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event)
+    {
+
+        if (!event.getAuthor().isBot() && event.isFromGuild())
+        {
+
+            try
+            {
+                Member member = event.getMessage().getMentions().getMembers().get(0);
+
+                // AFK Return
+                if (member.getEffectiveName().contains("(AFK)"))
+                {
+
+                    EmbedBuilder builder = new EmbedBuilder()
+                            .setDescription("Mentioned member is AFK, " + Objects.requireNonNull(event.getMember()).getAsMention() + "!")
+                            .setColor(Util.randColor());
+
+                    event.getChannel().sendMessageEmbeds(builder.build()).queue();
+                }
+            }
+            catch (Exception ignore) {}
         }
     }
 }
