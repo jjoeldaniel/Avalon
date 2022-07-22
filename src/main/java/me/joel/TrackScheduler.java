904848bf698b2e2b5e911d9b2d886d6312f707a6
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-22 00:42:53 -0700

    Edited code for increased readability

--- src/main/java/me/joel/TrackScheduler.java
+++ src/main/java/me/joel/TrackScheduler.java
@@ -8,31 +8,38 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class TrackScheduler extends AudioEventAdapter {
+public class TrackScheduler extends AudioEventAdapter
+{
 
     public final AudioPlayer audioPlayer;
     public final BlockingQueue<AudioTrack> queue;
 
-    public TrackScheduler(AudioPlayer audioPlayer) {
+    public TrackScheduler(AudioPlayer audioPlayer)
+    {
         this.audioPlayer = audioPlayer;
         this.queue = new LinkedBlockingQueue<>();
     }
 
-    public void queue(AudioTrack track) {
-        if (!this.audioPlayer.startTrack(track, true)) {
+    public void queue(AudioTrack track)
+    {
+        if (!this.audioPlayer.startTrack(track, true))
+        {
             this.queue.offer(track);
         }
     }
 
-    public void nextTrack() {
+    public void nextTrack()
+    {
         this.audioPlayer.startTrack(this.queue.poll(), false);
         MusicCommands.setSendNowPlaying(true);
         MusicCommands.sendNowPlaying(audioPlayer.getPlayingTrack(), MusicCommands.returnChannel());
     }
 
     @Override
-    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
-        if (endReason.mayStartNext) {
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason)
+    {
+        if (endReason.mayStartNext)
+        {
             nextTrack();
 
         }
