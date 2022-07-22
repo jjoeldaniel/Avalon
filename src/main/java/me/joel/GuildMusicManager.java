88e04993f2e54f5f36300dd8cb6ec716803bc473
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-22 00:49:10 -0700

    Edited code for increased readability

--- src/main/java/me/joel/GuildMusicManager.java
+++ src/main/java/me/joel/GuildMusicManager.java
@@ -3,20 +3,23 @@ package me.joel;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 
-public class GuildMusicManager {
+public class GuildMusicManager
+{
 
     public final AudioPlayer audioPlayer;
     public final TrackScheduler scheduler;
     private final AudioPlayerSendHandler sendHandler;
 
-    public GuildMusicManager(AudioPlayerManager manager) {
+    public GuildMusicManager(AudioPlayerManager manager)
+    {
         this.audioPlayer = manager.createPlayer();
         this.scheduler = new TrackScheduler((this.audioPlayer));
         this.audioPlayer.addListener(this.scheduler);
         this.sendHandler = new AudioPlayerSendHandler(this.audioPlayer);
     }
 
-    public AudioPlayerSendHandler getSendHandler() {
+    public AudioPlayerSendHandler getSendHandler()
+    {
         return this.sendHandler;
     }
 }
