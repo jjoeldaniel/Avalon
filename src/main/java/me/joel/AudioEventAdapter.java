fabfb3b30c81b0fe034ba569686efd673fd26acb
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-28 17:20:41 -0700

    Created AudioEventAdapter.java in support of new /loop command

---
+++ src/main/java/me/joel/AudioEventAdapter.java
@@ -0,0 +1,18 @@
+package me.joel;
+
+public class AudioEventAdapter extends com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter
+{
+
+    private static boolean isLoop = false;
+
+    public static void setLoop(boolean value)
+    {
+        isLoop = value;
+    }
+
+    public static boolean isLooping()
+    {
+        return isLoop;
+    }
+
+}
