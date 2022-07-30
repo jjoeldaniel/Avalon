600b578a7b2f6a3a5da1869c02be4f49823abbe3
Joel <100006388+joelrico@users.noreply.github.com>
2022-07-29 21:47:25 -0700

    Removed unused imports

--- src/main/java/me/joel/Paw.java
+++ src/main/java/me/joel/Paw.java
@@ -5,8 +5,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
