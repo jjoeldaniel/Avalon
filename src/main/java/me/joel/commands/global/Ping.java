2ea3714a0f5f2f2263a473f0074ed1d429b6a627
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-05 23:36:34 -0700

    Finished Ping.java

--- src/main/java/me/joel/commands/global/Ping.java
+++ src/main/java/me/joel/commands/global/Ping.java
@@ -1,4 +1,23 @@
 package me.joel.commands.global;
 
-public class Ping {
+import me.joel.Util;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Ping extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+
+        var invoke = event.getName();
+
+        if (invoke.equals("ping")) {
+            EmbedBuilder ping = new EmbedBuilder()
+                    .setTitle("Pong!")
+                    .setColor(Util.randColor());
+            event.replyEmbeds(ping.build()).setEphemeral(true).queue();
+        }
+    }
 }
