1db8bfc0ecfd91312c6d44545caa0fc82526e825
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-06 03:27:07 -0700

    Removed old code

--- src/main/java/me/joel/ReactMessages.java
+++ src/main/java/me/joel/ReactMessages.java
@@ -1,17 +1,13 @@
 package me.joel;
 
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import me.joel.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 
 public class ReactMessages extends ListenerAdapter {
@@ -28,54 +24,6 @@ public class ReactMessages extends ListenerAdapter {
 
             // Grabs user input
             String messageSent = event.getMessage().getContentRaw().toLowerCase();
-
-            // Now Playing
-//            if (Objects.requireNonNull(event.getMember()).getId().equals("971239438892019743") && messageSent.contains("now playing")) {
-//
-//                final AudioManager audioManager = Objects.requireNonNull(event.getGuild()).getAudioManager();
-//                AudioTrack track = PlayerManager.getINSTANCE().getMusicManager(audioManager.getGuild()).scheduler.queue.peek();
-//
-//                if (track == null) return;
-//
-//                // Time from ms to m:s
-//                long trackLength = track.getInfo().length;
-//                long minutes = (trackLength / 1000) / 60;
-//                long seconds = ((trackLength / 1000) % 60);
-//
-//                long hours = 0;
-//                if (minutes >= 60) {
-//                    while (minutes > 60) {
-//                        hours++;
-//                        minutes -= 60;
-//                    }
-//                }
-//
-//                String songHours = String.valueOf(hours);
-//                if (hours < 10) songHours = "0" + minutes;
-//
-//                String songMinutes = String.valueOf(minutes);
-//                if (minutes < 10) songMinutes = "0" + minutes;
-//
-//                String songSeconds = String.valueOf(seconds);
-//                if (seconds < 10) songSeconds = "0" + seconds;
-//
-//                EmbedBuilder builder = new EmbedBuilder()
-//                        .setColor(Util.randColor())
-//                        .setAuthor("Now Playing")
-//                        .setTitle(track.getInfo().title, track.getInfo().uri)
-//                        .setDescription("`[0:00 / [" + songMinutes + ":" + songSeconds + "]`");
-//
-//                if (hours > 0) {
-//                    builder.setDescription("`[0:00 / [" + songHours + ":" + songMinutes + ":" + songSeconds + "]`");
-//                }
-//
-//                if (track.getInfo().uri.contains("youtube.com")) {
-//                    builder.setThumbnail(PlayerManager.getThumbnail(track.getInfo().uri));
-//                }
-//
-//                event.getChannel().sendMessageEmbeds(builder.build()).queue();
-//                return;
-//            }
 
             // Check for bot
             if (event.getMember() == null) return;
