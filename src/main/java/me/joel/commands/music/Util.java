5178383a4e84b74b2c488985c48478c9c7d3ad8e
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-05 19:26:14 -0700

    Added getMember

---
+++ src/main/java/me/joel/commands/music/Util.java
@@ -0,0 +1,45 @@
+package me.joel.commands.music;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Utility class
+ */
+public class Util extends ListenerAdapter {
+
+    private static Member member;
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        member = event.getMember();
+    }
+
+    /**
+     * @return Event member
+     */
+    public static Member getMember() {
+        return member;
+    }
+
+    /**
+     * User is required to be in VC
+     */
+    static final EmbedBuilder VCRequirement = new EmbedBuilder()
+            .setColor(me.joel.Util.randColor())
+            .setDescription("You need to be in a voice channel to use this command!")
+            .setFooter("Use /help for a list of music commands!");
+
+    /**
+     * User is required to be in same VC as bot
+     */
+    static final EmbedBuilder sameVCRequirement = new EmbedBuilder()
+            .setColor(me.joel.Util.randColor())
+            .setDescription("You need to be in the same voice channel to use this command!")
+            .setFooter("Use /help for a list of music commands!");
+
+
+}
