d5dc2e78147abf01314d0cc9fbecb058a26a6ceb
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-10 03:59:22 -0700

    Added initial deal

--- src/main/java/me/joel/games/Blackjack/Blackjack.java
+++ src/main/java/me/joel/games/Blackjack/Blackjack.java
@@ -9,9 +9,24 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.TimeUnit;
+
 public class Blackjack extends ListenerAdapter {
 
     User user;
+    String messageID;
+
+    Button rules = Button.primary("rules", "See Rules");
+    Button play = Button.success("play", "Play");
+    Button menu = Button.primary("menu", "Menu");
+
+    public EmbedBuilder menu() {
+        return new EmbedBuilder()
+                .setColor(Util.randColor())
+                .setTitle("Blackjack Menu (WIP)")
+                .setDescription("Press one of the buttons below to start playing!")
+                .setColor(Util.randColor());
+    }
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
@@ -21,16 +36,8 @@ public class Blackjack extends ListenerAdapter {
 
         if (invoke.equalsIgnoreCase("blackjack")) {
 
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setColor(Util.randColor())
-                    .setTitle("Blackjack Menu (WIP)")
-                    .setDescription("Press one of the buttons below to start playing!")
-                    .setColor(Util.randColor());
-
-            event.replyEmbeds(builder.build()).addActionRow(
-                    Button.primary("rules", "See Rules"),
-                    Button.success("play", "Play")
-                    ).queue();
+            EmbedBuilder builder = menu();
+            event.replyEmbeds(builder.build()).addActionRow(rules, menu.asDisabled(), play).queue();
         }
     }
 
@@ -38,6 +45,8 @@ public class Blackjack extends ListenerAdapter {
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
 
         var invoke = event.getComponentId();
+        messageID = event.getMessageId();
+        System.out.println(invoke);
 
         // If interaction member != /blackjack member
         if (event.getUser() != user) {
@@ -56,7 +65,10 @@ public class Blackjack extends ListenerAdapter {
                         .addField("The premise of the game is simple..", "The first player to 21 wins.\nWatch out though, if you go over 21, you lose!", false)
                         .addField("Special card values are as follows:", "Ace = 1/11, Jack, King, and Queen are 10", false)
                         .setColor(Util.randColor());
-                event.editMessageEmbeds(builder.build()).queue();
+
+                event.editMessageEmbeds(builder.build())
+                        .setActionRow(rules.asDisabled(), menu.asEnabled(), play.asEnabled())
+                        .queue();
             }
             case ("play") -> {
                 String card1 = Deck.randomCard();
@@ -70,6 +82,27 @@ public class Blackjack extends ListenerAdapter {
                         .addField("Card 2: " + card2, "", true)
                         .setColor(Util.randColor());
 
+                event.editMessageEmbeds(builder.build())
+                        .setActionRow(rules.asDisabled(), play.asDisabled(), menu.asEnabled())
+                        .queue();
+
+                if (total == 21) {
+                    event.getHook().editMessageEmbedsById(messageID, builder.build())
+                            .setActionRow(rules.asDisabled(), play.asDisabled(), menu.asDisabled())
+                            .queue();
+
+                    EmbedBuilder hit21 = new EmbedBuilder()
+                            .setTitle("Blackjack!")
+                            .setDescription("You have won 100 credits")
+                            .setColor(Util.randColor());
+
+                    event.getHook().editMessageEmbedsById(messageID, hit21.build())
+                            .setActionRow(rules.asEnabled(), menu.asEnabled(), play.asEnabled())
+                            .queueAfter(3, TimeUnit.SECONDS);
+                }
+            }
+            case ("menu") -> {
+                EmbedBuilder builder = menu();
                 event.editMessageEmbeds(builder.build()).queue();
             }
         }
