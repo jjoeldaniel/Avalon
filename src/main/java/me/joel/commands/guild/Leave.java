157d624fe7152e581c3809ca270d58fe4a055f45
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-11 14:34:56 -0700

    Added case for when bot is not in VC

--- src/main/java/me/joel/commands/guild/Leave.java
+++ src/main/java/me/joel/commands/guild/Leave.java
@@ -2,11 +2,14 @@ package me.joel.commands.guild;
 
 import me.joel.commands.music.Util;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class Leave extends ListenerAdapter {
 
@@ -27,11 +30,21 @@ public class Leave extends ListenerAdapter {
             // JDA AudioManager
             final AudioManager audioManager = event.getGuild().getAudioManager();
 
+            Member bot = event.getGuild().retrieveMemberById("971239438892019743").complete();
+
+            if (!bot.getVoiceState().inAudioChannel()) {
+                EmbedBuilder noVC = new EmbedBuilder()
+                        .setDescription("The bot is not in a VC!")
+                        .setColor(Color.red);
+
+                event.replyEmbeds(noVC.build()).setEphemeral(true).queue();
+            }
+
             final VoiceChannel memberChannel = (VoiceChannel) event.getMember().getVoiceState().getChannel();
             audioManager.closeAudioConnection();
 
             builder = new EmbedBuilder()
-                .setColor(me.joel.Util.randColor())
+                .setColor(Color.red)
                 .setDescription("Left " + memberChannel.getName() + "!");
 
             event.replyEmbeds(builder.build()).setEphemeral(false).queue();
