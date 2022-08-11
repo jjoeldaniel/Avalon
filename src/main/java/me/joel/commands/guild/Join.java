ac4fd4bf182cffe95c3564747726c799f9aac210
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-11 14:27:07 -0700

    Removed unused imports

--- src/main/java/me/joel/commands/guild/Join.java
+++ src/main/java/me/joel/commands/guild/Join.java
@@ -9,8 +9,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 public class Join extends ListenerAdapter {
 
     @Override
@@ -20,51 +18,38 @@ public class Join extends ListenerAdapter {
 
         if (invoke.equals("join")) {
 
-            // Avalon
-            Member bot = Objects.requireNonNull(event.getGuild()).getMemberById("971239438892019743");
-            assert bot != null;
+            EmbedBuilder builder = Util.compareVoice(event.getMember(), Util.getAvalon(event.getGuild()));
 
-            // Checks requester voice state
-            if (!Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getVoiceState()).inAudioChannel()) {
-                event.replyEmbeds(Util.VCRequirement.build()).setEphemeral(true).queue();
-                return;
-            }
-
-            // Compare JDA and member voice state
-            if (Objects.requireNonNull(bot.getVoiceState()).inAudioChannel()) {
-                long memberVC = Objects.requireNonNull(Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getVoiceState()).getChannel()).getIdLong();
-                long botVC = Objects.requireNonNull(bot.getVoiceState().getChannel()).getIdLong();
-
-                if (!(botVC == memberVC)) {
-                    event.replyEmbeds(Util.sameVCRequirement.build()).setEphemeral(true).queue();
-                    return;
-                }
-            }
-
-            // JDA AudioManager
-            final AudioManager audioManager = Objects.requireNonNull(event.getGuild()).getAudioManager();
-
-            final VoiceChannel memberChannel = (VoiceChannel) event.getMember().getVoiceState().getChannel();
-
-            if (bot.getVoiceState().getChannel() == memberChannel) {
-                assert memberChannel != null;
-                EmbedBuilder builder = new EmbedBuilder()
-                        .setColor(me.joel.Util.randColor())
-                        .setDescription("Already in " + memberChannel.getName() + "!");
-
+            if (builder != null) {
                 event.replyEmbeds(builder.build()).setEphemeral(true).queue();
                 return;
             }
 
+            // JDA AudioManager
+            final AudioManager audioManager = event.getGuild().getAudioManager();
+
+            final VoiceChannel memberChannel = (VoiceChannel) event.getMember().getVoiceState().getChannel();
+
+            Member bot = event.getGuild().retrieveMemberById("971239438892019743").complete();
+
+            if (bot.getVoiceState().inAudioChannel()) {
+                if (bot.getVoiceState().getChannel() == memberChannel) {
+                    EmbedBuilder builder1 = new EmbedBuilder()
+                            .setColor(me.joel.Util.randColor())
+                            .setDescription("Already in " + memberChannel.getName() + "!");
+
+                    event.replyEmbeds(builder1.build()).setEphemeral(true).queue();
+                    return;
+                }
+            }
 
             audioManager.openAudioConnection(memberChannel);
 
-            assert memberChannel != null;
-            EmbedBuilder builder = new EmbedBuilder()
+            EmbedBuilder builder1 = new EmbedBuilder()
                     .setColor(me.joel.Util.randColor())
                     .setDescription("Joined " + memberChannel.getName() + "!");
 
-            event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+            event.replyEmbeds(builder1.build()).setEphemeral(true).queue();
         }
     }
 }
