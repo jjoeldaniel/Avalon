53c69088f3f8bac46d4ea158fd29ab78300d10ec
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-11 13:01:03 -0700

    Format

--- src/main/java/me/joel/commands/music/Shuffle.java
+++ src/main/java/me/joel/commands/music/Shuffle.java
@@ -2,12 +2,9 @@ package me.joel.commands.music;
 
 import me.joel.lavaplayer.AudioEventAdapter;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 public class Shuffle extends ListenerAdapter {
 
@@ -16,33 +13,19 @@ public class Shuffle extends ListenerAdapter {
 
         var invoke = event.getName();
 
+        if (event.getGuild() == null) return;
+
         if (invoke.equals("shuffle")) {
 
-            // Avalon
-            Member bot = Util.getAvalon();
+            EmbedBuilder builder;
+            builder = Util.compareVoice(event.getMember(), Util.getAvalon(event.getGuild()));
 
-            // Checks requester voice state
-            if (!Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getVoiceState()).inAudioChannel()) {
-                AudioEventAdapter.setLoop(false);
-                event.replyEmbeds(Util.VCRequirement.build()).setEphemeral(true).queue();
+            if (builder != null) {
+                event.replyEmbeds(builder.build()).setEphemeral(true).queue();
                 return;
             }
 
-            // Compare JDA and member voice state
-            if (Objects.requireNonNull(bot.getVoiceState()).inAudioChannel()) {
-                long memberVC = Objects.requireNonNull(Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getVoiceState()).getChannel()).getIdLong();
-                long botVC = Objects.requireNonNull(bot.getVoiceState().getChannel()).getIdLong();
-
-                if (!(botVC == memberVC)) {
-                    event.replyEmbeds(Util.sameVCRequirement.build()).setEphemeral(true).queue();
-                    return;
-                }
-            }
-
-            if (AudioEventAdapter.isShuffling()) AudioEventAdapter.setShuffle(false);
-            else AudioEventAdapter.setShuffle(true);
-
-            EmbedBuilder builder;
+            AudioEventAdapter.setShuffle(!AudioEventAdapter.isShuffling());
 
             if (AudioEventAdapter.isLooping()) {
                 builder = new EmbedBuilder()
