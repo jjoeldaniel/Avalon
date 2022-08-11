6ca215c4aec7eeadbd4a44e82a434e671c46b5c1
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-10 23:27:04 -0700

    Removed unused import

--- src/main/java/me/joel/commands/music/Clear.java
+++ src/main/java/me/joel/commands/music/Clear.java
@@ -3,7 +3,6 @@ package me.joel.commands.music;
 import me.joel.lavaplayer.AudioEventAdapter;
 import me.joel.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
