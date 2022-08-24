9e50c519a18530e10df70ba537725c567837e281
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-23 20:55:41 -0700

    Added register guild commands on guild join

--- src/main/java/me/joel/commands/Register.java
+++ src/main/java/me/joel/commands/Register.java
@@ -2,6 +2,7 @@ package me.joel.commands;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.Command;
@@ -26,6 +27,48 @@ public class Register extends ListenerAdapter {
      */
     @Override
     public void onGuildReady(@NotNull GuildReadyEvent event) {
+
+        List<CommandData> guildCommandData = new ArrayList<>();
+
+        // general
+        guildCommandData.add(Commands.slash("whois", "Provides user information").addOption(OptionType.MENTIONABLE, "user", "Sends user info", true));
+        guildCommandData.add(Commands.slash("afk", "Sets AFK status"));
+        guildCommandData.add(Commands.slash("confess", "Sends anonymous confession").addOption(OptionType.STRING, "message", "Confession message", true));
+        guildCommandData.add(Commands.slash("join", "Request for bot to join VC"));
+        guildCommandData.add(Commands.slash("leave", "Request for bot to leave VC"));
+        guildCommandData.add(Commands.context(Command.Type.USER, "Get member avatar"));
+        guildCommandData.add(Commands.context(Command.Type.USER, "Get member info"));
+        guildCommandData.add(Commands.context(Command.Type.MESSAGE, "Translate message"));
+
+        // toggle
+        SubcommandData insults = new SubcommandData("insults", "Toggles insults");
+        SubcommandData gmgn = new SubcommandData("goodmorning_goodnight", "Toggles good morning and goodnight messages");
+        SubcommandData nowPlaying = new SubcommandData("now_playing", "Toggles Now Playing messages");
+
+        guildCommandData.add(Commands.slash("toggle", "Toggles bot features").addSubcommands(insults, gmgn, nowPlaying).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)));
+
+        // mod
+        guildCommandData.add(Commands.slash("broadcast", "Broadcasts message in selected channel").addOption(OptionType.CHANNEL, "channel", "Broadcast channel", true).addOption(OptionType.STRING, "message", "Broadcast Message", true));
+        guildCommandData.add(Commands.slash("poll", "Submits poll message").addOption(OptionType.STRING, "message", "Question that will be voted on", true).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)));
+        guildCommandData.add(Commands.slash("purge", "Purges up to 100 messages").addOption(OptionType.INTEGER, "number", "Number of messages to purge", true).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_MANAGE)));
+
+        // music
+        guildCommandData.add(Commands.slash("play", "Requests a song").addOption(OptionType.STRING, "song", "Accepts Spotify, YouTube, or Apple Music", true));
+        guildCommandData.add(Commands.slash("pause", "Pause playback"));
+        guildCommandData.add(Commands.slash("resume", "Resume playback"));
+        guildCommandData.add(Commands.slash("clear", "Clears queue"));
+        guildCommandData.add(Commands.slash("queue", "Displays music queue"));
+        guildCommandData.add(Commands.slash("playing", "Displays currently playing song"));
+        guildCommandData.add(Commands.slash("loop", "Loops currently playing song"));
+        guildCommandData.add(Commands.slash("shuffle", "Shuffles music queue"));
+        guildCommandData.add(Commands.slash("volume", "Sets bot volume").addOption(OptionType.STRING, "num", "Sets volume (between 1 and 100)", true));
+        guildCommandData.add(Commands.slash("skip", "Skips song").addOption(OptionType.INTEGER, "target", "Removes selected song from queue", false).addOption(OptionType.INTEGER, "num", "Removes \"x\" number of songs", false));
+        guildCommandData.add(Commands.slash("seek", "Sets song position").addOption(OptionType.INTEGER, "seconds", "Sets seconds", false).addOption(OptionType.INTEGER, "minutes", "Sets minutes", false).addOption(OptionType.INTEGER, "hours", "Sets hours", false));
+
+        event.getGuild().updateCommands().addCommands(guildCommandData).queue();
+    }
+    @Override
+    public void onGuildJoin(@NotNull GuildJoinEvent event) {
 
         List<CommandData> guildCommandData = new ArrayList<>();
 
