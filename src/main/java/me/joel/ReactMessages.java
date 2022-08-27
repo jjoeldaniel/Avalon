06c38359230400bb6ccbc3571042d8a0e3c314e3
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-26 17:46:08 -0700

    Removed unused imports

--- src/main/java/me/joel/ReactMessages.java
+++ src/main/java/me/joel/ReactMessages.java
@@ -7,10 +7,6 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class ReactMessages extends ListenerAdapter {
 
     @Override
