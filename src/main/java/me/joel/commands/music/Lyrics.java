05e06223b9f4da1a0bf5c96c9206763d93dcf5cc
Joel <100006388+joelrico@users.noreply.github.com>
2022-08-28 21:18:12 -0700

    Added Genius lyrics and /lyrics support

--- src/main/java/me/joel/commands/music/Lyrics.java
+++ src/main/java/me/joel/commands/music/Lyrics.java
@@ -2,6 +2,8 @@ package me.joel.commands.music;
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import core.GLA;
+import genius.SongSearch;
+import me.joel.Util;
 import me.joel.lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -25,22 +27,39 @@ public class Lyrics extends ListenerAdapter {
                         .setColor(Color.red)
                         .setDescription("No song is playing!");
                 event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+                return;
             }
             else if (!track.getInfo().uri.contains("spotify.com")) {
                 EmbedBuilder builder = new EmbedBuilder()
                         .setColor(Color.red)
                         .setDescription("Only Spotify tracks are supported, sorry!");
                 event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+                return;
             }
+
+            event.deferReply().queue();
 
             GLA gla = new GLA();
+            EmbedBuilder lyricsEmbed = new EmbedBuilder();
+            String lyrics;
 
             try {
-                String lyrics = gla.search(track.getInfo().title + " " + track.getInfo().author).getHits().get(0).fetchLyrics();
-                System.out.print(lyrics);
+                SongSearch.Hit hit = gla.search(track.getInfo().title + " " + track.getInfo().author).getHits().get(0);
+                lyrics = hit.fetchLyrics();
+                lyricsEmbed.setThumbnail(hit.getThumbnailUrl());
+                lyricsEmbed.setTitle(hit.getTitleWithFeatured() + " - " + hit.getArtist().getName());
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                EmbedBuilder builder = new EmbedBuilder()
+                        .setColor(Color.red)
+                        .setDescription("No lyrics were found!");
+                event.getHook().sendMessageEmbeds(builder.build()).setEphemeral(true).queue();
+                return;
             }
+
+            lyricsEmbed.setColor(Util.randColor());
+            lyricsEmbed.setDescription(lyrics);
+
+            event.getHook().sendMessageEmbeds(lyricsEmbed.build()).queue();
         }
     }
 }
