b0e6a5ebfd5fe5773e580ac9ce321e22db33efc3
Joel <100006388+joelrico@users.noreply.github.com>
2022-10-02 01:33:03 -0700

    Removed activity timeout

--- src/main/java/me/joel/commands/music/Activity.java
+++ src/main/java/me/joel/commands/music/Activity.java
@@ -1,105 +1,13 @@
 package me.joel.commands.music;
 
-import me.joel.lavaplayer.AudioEventAdapter;
-import me.joel.lavaplayer.PlayerManager;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceGuildDeafenEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Activity extends ListenerAdapter {
-
-    @Override
-    public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
-
-        Member bot = event.getGuild().getSelfMember();
-        AudioChannel channel = event.getChannelLeft();
-
-        // Wait 3 minutes
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        Runnable task = () -> {
-
-            // Clear queue
-            if (PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).player.getPlayingTrack() != null) PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).player.destroy();
-            PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).scheduler.queue.clear();
-
-            // Disable shuffle/loop
-            AudioEventAdapter.setLoop(false);
-            AudioEventAdapter.setShuffle(false);
-
-            // Close connection
-            event.getGuild().getAudioManager().closeAudioConnection();
-
-            // Send message
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setColor(Color.red)
-                    .setTitle("Leaving inactive channel..")
-                    .setDescription("Music queue is cleared\nShuffle/Loop are disabled");
-
-            ((VoiceChannel) channel).sendMessageEmbeds(builder.build()).queue();
-        };
-
-        if (channel.getMembers().contains(bot) && channel.getMembers().size() == 1) {
-            executor.schedule(task, 30, TimeUnit.SECONDS);
-            executor.shutdown();
-        }
-    }
-
-    @Override
-    public void onGuildVoiceMove(@NotNull GuildVoiceMoveEvent event) {
-        Member bot = event.getGuild().getSelfMember();
-        AudioChannel channel = null;
-
-        // Get VC
-        if (bot.getVoiceState().inAudioChannel()) {
-            channel = bot.getVoiceState().getChannel();
-        }
-
-        if (channel == null) return;
-
-        // Wait 3 minutes
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        VoiceChannel finalChannel = (VoiceChannel) channel;
-        Runnable task = () -> {
-
-            // Clear queue
-            if (PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).player.getPlayingTrack() != null)
-                PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).player.destroy();
-            PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).scheduler.queue.clear();
-
-            // Disable shuffle/loop
-            AudioEventAdapter.setLoop(false);
-            AudioEventAdapter.setShuffle(false);
-
-            // Close connection
-            event.getGuild().getAudioManager().closeAudioConnection();
-
-            // Send message
-            EmbedBuilder builder = new EmbedBuilder()
-                    .setColor(Color.red)
-                    .setTitle("Leaving inactive channel..")
-                    .setDescription("Music queue is cleared\nShuffle/Loop are disabled");
-
-            finalChannel.sendMessageEmbeds(builder.build()).queue();
-        };
-
-        // If in VC and channel is empty aside from bot
-        if (channel.getMembers().contains(bot) && channel.getMembers().size() == 1) {
-            executor.schedule(task, 30, TimeUnit.SECONDS);
-            executor.shutdown();
-        }
-    }
 
     @Override
     public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
